7a1111cf742e4bf366f6c2585cf9568d6a28055a
pratik giri <pratikgiri2015@gmail.com>
2022-11-06 22:14:03 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+    
+ <Navbar title="textUtils" home="Home" link="Link"/>
+ 
+ <textform></textform>
+ <hello></hello>
+ 
+ </> 
+  );
+}
+
+export default App;
